a7443d5882eb8952fb705252f234766fd26be045
LastToKnow <54257598+burydmitri@users.noreply.github.com>
2024-01-25 13:47:00 +0300

    chore: move the AppContent to a separate component

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,22 +1,15 @@
 <template>
     <main class="flex justify-center">
-      <div class="w-full max-w-screen-md p-5 shadow-xl rounded-lg">
 
-        <h1 class="text-center text-5xl text-text-color mb-20" @click="toggleShowAverage">
-          My weather app
-        </h1>
-
-        <WeatherSection />
-
-      </div>
+      <AppContent />
 
       <AverageButton class="absolute top-5 right-5 shadow-md"/>
-      
+
     </main>
 </template>
 
 <script setup>
-  import WeatherSection from './components/weatherSection.vue'
+  import AppContent from './components/AppContent.vue';
   import AverageButton from './components/AverageButton.vue'
 </script>
 
---
+++ vite-project/src/components/AppContent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="w-full max-w-screen-md p-5 shadow-xl rounded-lg">
+
+        <h1 class="text-center text-5xl text-text-color mb-20">
+            My weather app
+        </h1>
+
+        <WeatherSection />
+
+    </div>
+</template>
+
+<script setup>
+  import WeatherSection from './weatherSection.vue'
+</script>
